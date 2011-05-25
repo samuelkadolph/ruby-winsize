a04aa35a9ef603c0d557c324256e6562b04e085c
Samuel Kadolph <samuel@kadolph.com>
2011-05-24 17:48:29 -0700

    Added more extconf.rb checks

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,7 @@
 require "mkmf"
 
-abort("ioctl is missing.\n\n") unless have_header("sys/ioctl.h")
+abort("sys/ioctl.h is required.") unless have_header("sys/ioctl.h")
+abort("TIOCGWINSZ is required.") unless have_const("TIOCGWINSZ")
+abort("TIOCSWINSZ is required.") unless have_const("TIOCSWINSZ")
 
 create_makefile("winsize")
